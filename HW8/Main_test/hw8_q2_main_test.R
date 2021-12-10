a93edd1eac8ed8dc24e933d8a8caa6b363ff35b8
Hyeseon <93330327+Emptyroom4@users.noreply.github.com>
2021-12-09 22:13:30 -0500

    Add files via upload

---
+++ HW8/Main_test/hw8_q2_main_test.R
@@ -0,0 +1,16 @@
+
+#Q2_main test
+#Hyeseon Seo
+
+set.seed(1234567890)
+
+test_that('ks_test', {
+  set.seed(1234567890)
+  n1 = rejection_sampling(100, dnorm, function(x) dt(x,1), function(n) rt(n,1), 1.52)
+  n2 = rejection_sampling(100, dnorm, function(x) 1/2*exp(-abs(x)), function(n) rexp(n,1)*(2*rbinom(n,1,0.5)-1), 1.32)
+  
+  ks_test1 = ks.test(n1, pnorm)
+  ks_test2 = ks.test(n2, pnorm)
+  expect_gt(ks_test1$p.value, 0.01)
+  expect_gt(ks_test2$p.value, 0.01)
+})
---
+++ HW8/Main_test/hw8_q3_main_test.R
@@ -0,0 +1,17 @@
+
+#Q3. main test
+# Hyeseon Seo
+
+
+library(testthat)
+
+test_that('ks_test', {
+  h = function(x,mu,sigma) -(x-mu)^2/(2*sigma^2)-0.5*log(2*pi*sigma^2)
+  interval = c(-Inf,Inf)
+  mu = 3
+  sigma = 2
+  x = mu+c(-1,1)*sigma
+  n = 1000
+  out = adaptive_rejection_sampling(n, h, interval, x, mu=mu, sigma=sigma) 
+  expect_gt(ks.test(out, pnorm, mean=mu, sd=sigma)$p.value, 0.01)
+})
