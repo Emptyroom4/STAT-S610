515857a12d1473ab63ffc34a433f02989df045dc
Hyeseon <93330327+Emptyroom4@users.noreply.github.com>
2021-12-16 23:12:21 -0500

    Add files via upload

---
+++ Final/Q3/final_q3.R
@@ -0,0 +1,40 @@
+library(testthat)
+
+eigen_fun = function(X,tol=.Machine$double.eps^0.75){
+  if(!isSymmetric(X)) stop("x is not symmetric")
+  d = ncol(X)
+  U = diag(1,d,d,FALSE)
+  while(TRUE){
+    did_we_update = FALSE
+    for(i in 1:(d-1)){
+      for(j in (i+1):d){
+        # test whether abs(X[i,j]) is too large
+        if(abs(X[i,j])>tol){
+          # if so, get the Givens Rotation matrix R
+          if (X[i,i] == X[j,j]){
+            theta = pi/4
+          }else {
+            theta = 1/2*atan(2*X[j,i] /-(X[i,i]- X[j,j]))
+          }
+          s = sin(theta)
+          c = cos(theta)
+          R = matrix(c(s,c,-c,s),2,2)
+          # and update X and U
+          X = R%*%X%*%t(R)
+          U = U%*%R
+
+          # track that in this sweep through we have done at least one update
+          did_we_update = TRUE
+        }
+        
+      }
+    }
+    if(!did_we_update) break
+  }
+  values=diag(X)
+  sorted_values = sort(values,index.return=TRUE,decreasing=TRUE)
+  values = sorted_values$x
+  vectors = U[,sorted_values$ix]
+  return(list(values=values, vectors=vectors))
+}
+
